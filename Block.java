a08ab75c0a8c8fbf088add2d0e5185666a152e50
Dan Lenz <71292133+7alen7@users.noreply.github.com>
2021-04-08 15:15:35 -0500

    Create Block.java

---
+++ Block.java
@@ -0,0 +1,189 @@
+package tetris;
+
+import java.util.Random;
+
+public class Block {
+	
+	public static final int NO_BLOCK = 0;
+
+	public static final int SQUARE_FIGURE = 5;
+
+	      /**
+	       * A figure constant used to create a figure forming a line.
+	       */
+	public static final int LINE_FIGURE = 2;
+
+	      /**
+	       * A figure constant used to create a figure forming an "S".
+	       */
+	public static final int S_FIGURE = 3;
+
+	      /**
+	       * A figure constant used to create a figure forming a "Z".
+	       */
+	public static final int Z_FIGURE = 4;
+
+	      /**
+	       * A figure constant used to create a figure forming a right angle.
+	       */
+	public static final int RIGHT_ANGLE_FIGURE = 1;
+
+	      /**
+	       * A figure constant used to create a figure forming a left angle.
+	       */
+	public static final int LEFT_ANGLE_FIGURE = 6;
+
+	      /**
+	       * A figure constant used to create a figure forming a triangle.
+	       */
+	public static final int TRIANGLE_FIGURE = 7;
+
+
+		private int type;
+	    private int[][] x_y;
+
+	    private int xpos = 0;
+	    private int ypos = 0;
+	    
+	    public Block() {
+
+	        x_y = new int[4][2];
+	        createBlock(NO_BLOCK);
+	    }
+	    
+	    
+
+	    void createBlock(int blockType) {
+
+	        int[][][] x_yTable = new int[][][]{
+	                {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+	                {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
+	                {{0, -1}, {0, 0}, {1, 0}, {1, 1}},
+	                {{0, -1}, {0, 0}, {0, 1}, {0, 2}},
+	                {{-1, 0}, {0, 0}, {1, 0}, {0, 1}},
+	                {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+	                {{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
+	                {{1, -1}, {0, -1}, {0, 0}, {0, 1}}
+	        };
+
+	        for (int i = 0; i < 4; i++) {
+
+	            System.arraycopy(x_yTable[blockType], 0, x_y, 0, 4);
+	        }
+
+	        type = blockType;
+	    }
+	    
+	    private void setX(int index, int x) {
+
+	        x_y[index][0] = x;
+	    }
+
+	    private void setY(int index, int y) {
+
+	        x_y[index][1] = y;
+	    }
+
+	    public int get_x(int index) {
+
+	        return x_y[index][0];
+	    }
+
+	    public int get_y(int index) {
+
+	        return x_y[index][1];
+	    }
+	    
+	    public int getXpos()
+	    {
+	    	return xpos;
+	    }
+	    
+	    public int getYpos()
+	    {
+	    	return ypos;
+	    }
+
+	    public int getType()
+	    {
+	    	return type;
+	    }
+
+	    
+	    void randomBlock() 
+	    {
+
+	        Random r = new Random();
+	        int x = Math.abs(r.nextInt()) % 7 + 1;
+
+	        createBlock(x);
+	    }
+
+	    public int minX() 
+	    {
+
+	        int m = x_y[0][0];
+
+	        for (int i = 0; i < 4; i++) {
+
+	            m = Math.min(m, x_y[i][0]);
+	        }
+
+	        return m;
+	    }
+
+
+	    int minY() 
+	    {
+
+	        int m = x_y[0][1];
+
+	        for (int i = 0; i < 4; i++) {
+
+	            m = Math.min(m, x_y[i][1]);
+	        }
+
+	        return m;
+	    }
+
+	    public Block rotateCounter() 
+	    {
+
+	        if (type == SQUARE_FIGURE) {
+
+	            return this;
+	        }
+
+	        Block result = new Block();
+	        result.type = type;
+
+	        for (int i = 0; i < 4; i++) {
+
+	            result.setX(i, get_y(i));
+	            result.setY(i, -1 * get_x(i));
+	        }
+
+	        return result;
+	    }
+
+	    public Block rotateClockwise() 
+	    {
+
+	        if (type == SQUARE_FIGURE) {
+
+	            return this;
+	        }
+
+	        Block result = new Block();
+	        result.type = type;
+
+	        for (int i = 0; i < 4; i++) {
+
+	            result.setX(i, -1 * get_y(i));
+	            result.setY(i, get_x(i));
+	        }
+
+	        return result;
+	    }
+}
+
