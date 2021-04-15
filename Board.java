0f73d3b431da923ff590e96e026181c3f0485fc0
dandarnell <dandarnell@users.noreply.github.com>
2021-04-15 07:28:23 -0500

    Create Board.java
    Created Board.java and added skeleton functions to it.

---
+++ Board.java
@@ -0,0 +1,43 @@
+package tetris;
+
+public class Board {
+    private int width;
+    private int height;
+    private int linesRemoved;
+    
+    public Board(int width, int height) {
+        
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+    
+    public int getHeight() {
+        return height;
+    }
+    
+    public int getLinesRemoved() {
+        return linesRemoved;
+    }
+    
+    public boolean emptySquare() {
+        
+    }
+    
+    public boolean fullLine() {
+        
+    }
+    
+    public void removeLine() {
+        
+    }
+    
+    public void shiftDown() {
+        
+    }
+    
+    public void resetBoard() {
+        
+    }
+}
