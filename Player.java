d2a1c741b884fca15222a429b0b4d751a6f0a3d0
ghearne19 <82472054+ghearne19@users.noreply.github.com>
2021-04-19 19:30:27 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,17 +3,17 @@ package tetris;
 public class Player {
 	private String name;
 	
-	Player(String playerName)
+	Player(String playerName)//constructor for Player class
 	{
-		name = playerName;
+		name = playerName;//Sets player name to temporary parameter
 	}
 	
-	public void setPlayerName(String n)
+	public void setPlayerName(String n)//Creates the name for the player 
 	{
 		name = n;
 	}
 	
-	public String getName()
+	public String getName()//Returns name to the function call and allows the name to be accessed
 	{
 		return this.name;
 	}
