9b2c2cafa46e8ed27454b4f66782bd4e116c15a9
Dan Lenz <71292133+7alen7@users.noreply.github.com>
2021-04-23 11:33:08 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,18 +1,37 @@
 package tetris;
-//This class is responsible creating the player and holding the name 
+/**
+ * Player Object Class
+ * 
+ * <p> Defines a getter and setter function for manipulating the current Player's name
+ * @author CS321 Group 3
+ * @version 3rd attempt to get this project to work.
+ *
+ */ 
 public class Player {
 	private String name;
 	
+	/**
+	 * Constructor for creating a Player object
+	 * @param playerName: String to be set as the player's name
+	 */
 	Player(String playerName)//constructor for Player class
 	{
 		name = playerName;//Sets player name to temporary parameter
 	}
 	
+	/**
+	 * Method used to change the player's current name
+	 * @param n: String to be set as player's name
+	 */
 	public void setPlayerName(String n)//Creates the name for the player 
 	{
 		name = n;
 	}
 	
+	/**
+	 * Method to get the player's name
+	 * @return The player's name
+	 */
 	public String getName()//Returns name to the function call and allows the name to be accessed
 	{
 		return this.name;
