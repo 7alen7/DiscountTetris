1714cde67e889dfaf41569632c4a264bcc52ef35
Dan Lenz <71292133+7alen7@users.noreply.github.com>
2021-04-23 11:56:41 -0500

    Update startTetris.java

--- startTetris.java
+++ startTetris.java
@@ -5,12 +5,17 @@ import java.io.IOException;
 /**
  * Start Tetris Class
  * 
- * <p> Used to start a game of tetris by creating a new Game object and using its start method.
+ * <p> Used to start a game of Discount Tetris by creating a new Game object and using its start method.
  * @author CS321 Group 3
  * @version 3rd attempt to get this project to work.
  *
  */ 
 public class startTetris {
+	/**
+	 * Main function used to run the Tetris game
+	 * @param args Not used in this case
+	 * @throws IOException if scores.txt cannot be opened or created
+	 */
 	public static void main(String[] args) throws IOException
 	{
 		Game game = new Game(); // create new game
