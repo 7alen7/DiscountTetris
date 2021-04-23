1996f0ee2fc1ea0f7d0181cfc920662c5cbb7bd8
Dan Lenz <71292133+7alen7@users.noreply.github.com>
2021-04-23 11:35:08 -0500

    Update startTetris.java

--- startTetris.java
+++ startTetris.java
@@ -2,13 +2,21 @@ package tetris;
 
 import java.io.IOException;
 
+/**
+ * Start Tetris Class
+ * 
+ * <p> Used to start a game of tetris by creating a new Game object and using its start method.
+ * @author CS321 Group 3
+ * @version 3rd attempt to get this project to work.
+ *
+ */ 
 public class startTetris {
 	public static void main(String[] args) throws IOException
 	{
-		Game game = new Game();
+		Game game = new Game(); // create new game
 		
-		game.start();
-		game.setVisible(true);
+		game.start(); // start the game
+		game.setVisible(true); // allow you to see the game
 	}
 
 }
